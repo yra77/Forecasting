332ed593312b624529303c9d4dd7da16cea632ad
Yura <kceni.kceni.19@gmail.com>
2021-07-26 20:37:09 +0300

    Crypto forecast

--- Forecasting.py
+++ Forecasting.py
@@ -1,9 +1,151 @@
 
+import os
+import IPython
+import IPython.display
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import pandas as pd
+import seaborn as sns
+from pylab import rcParams
+import matplotlib.pyplot as plt
+from matplotlib import rc
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.layers import Bidirectional, Dropout, Activation, Dense, LSTM
+from tensorflow.python.keras.layers import CuDNNLSTM
+from tensorflow.keras.models import Sequential
+
+#%matplotlib inline
+
+sns.set(style='whitegrid', palette='muted', font_scale=1.5)
+
+rcParams['figure.figsize'] = 14, 8
+
+RANDOM_SEED = 42
+
+np.random.seed(RANDOM_SEED)
+
+
+df = pd.read_csv('BTC-USD.csv', parse_dates=['Date'])
+
+df.head()
+
+ax = df.plot(x='Date', y='Close');
+ax.set_xlabel("Date")
+ax.set_ylabel("Close Price (USD)")
+plt.show()
+
+scaler = MinMaxScaler()
+close_price = df.Close.values.reshape(-1, 1)
+scaled_close = scaler.fit_transform(close_price)
+
+scaled_close.shape
+
+np.isnan(scaled_close).any()
+
+scaled_close = scaled_close[~np.isnan(scaled_close)]
+
+scaled_close = scaled_close.reshape(-1, 1)
+
+np.isnan(scaled_close).any()
 
 
 
+SEQ_LEN = 100
+
+def to_sequences(data, seq_len):
+    d = []
+
+    for index in range(len(data) - seq_len):
+        d.append(data[index: index + seq_len])
+
+    return np.array(d)
+
+def preprocess(data_raw, seq_len, train_split):
+
+    data = to_sequences(data_raw, seq_len)
+
+    num_train = int(train_split * data.shape[0])
+
+    X_train = data[:num_train, :-1, :]
+    y_train = data[:num_train, -1, :]
+
+    X_test = data[num_train:, :-1, :]
+    y_test = data[num_train:, -1, :]
+
+    return X_train, y_train, X_test, y_test
 
 
+X_train, y_train, X_test, y_test = preprocess(scaled_close, SEQ_LEN, train_split = 0.95)
+
+
+print(X_train.shape)
+
+print(X_test.shape)
+
+
+DROPOUT = 0.2
+WINDOW_SIZE = SEQ_LEN - 1
+
+model = keras.Sequential()
+
+model.add(Bidirectional(LSTM(WINDOW_SIZE, return_sequences=True),
+                        input_shape=(WINDOW_SIZE, X_train.shape[-1])))#,activation='tanh',recurrent_activation='sigmoid'
+model.add(Dropout(rate=DROPOUT))
+
+model.add(Bidirectional(LSTM((WINDOW_SIZE * 2), return_sequences=True)))
+model.add(Dropout(rate=DROPOUT))
+
+model.add(Bidirectional(LSTM(WINDOW_SIZE, return_sequences=False)))
+
+model.add(Dense(units=1))
+
+model.add(Activation('linear'))
+
+
+model.compile(
+    loss='mean_squared_error', 
+    optimizer='adam'
+)
+
+BATCH_SIZE = 64
+
+history = model.fit(
+    X_train, 
+    y_train, 
+    epochs=15, 
+    batch_size=BATCH_SIZE, 
+    shuffle=False,
+    validation_split=0.1
+)
+
+model.evaluate(X_test, y_test)
+
+#plt.plot(history.history['loss'])
+#plt.plot(history.history['val_loss'])
+#plt.title('model loss')
+#plt.ylabel('loss')
+#plt.xlabel('epoch')
+#plt.legend(['train', 'test'], loc='upper left')
+#plt.show()
+
+y_hat = model.predict(X_test)
+
+model.save('Model')
+model.save("Model/model_26_07.h5")
+
+y_test_inverse = scaler.inverse_transform(y_test)
+y_hat_inverse = scaler.inverse_transform(y_hat)
+ 
+plt.plot(y_test_inverse, label="Actual Price", color='green')
+plt.plot(y_hat_inverse, label="Predicted Price", color='red')
+ 
+plt.title('Bitcoin price prediction')
+plt.xlabel('Time [days]')
+plt.ylabel('Price')
+plt.legend(loc='best')
+ 
+plt.show();
 
 
 
